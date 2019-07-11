99713ff4ab3c1c7c43041a921d20766a8196f089
Matthew Secrist <matthew@matthewsecrist.net>
2019-07-10 23:37:46 -0500

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule Fxnk.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :fxnk,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+
+      # Docs
+      name: "Fxnk",
+      source_url: "https://github.com/matthewsecrist/fxnk",
+      homepage_url: "http://matthewsecrist.dev/fxnk",
+      docs: [
+        main: "Fxnk",
+        logo: "logo.png",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.19", only: [:dev], runtime: false}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
