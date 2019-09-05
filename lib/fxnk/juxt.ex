3e1bfb843aee74ec33a06f5150ae0301b88da289
Matthew Secrist <matthew@matthewsecrist.net>
2019-09-04 22:59:29 -0500

    fix doctests

--- lib/fxnk/juxt.ex
+++ lib/fxnk/juxt.ex
@@ -11,11 +11,11 @@ defmodule Fxnk.Juxt do
   `juxt/1` takes list of functions and returns a curried juxt.
 
   ## Example
-      iex> minmax = Fxnk.Juxt.juxt([&Fxnk.Math.Min.min/1, &Fxnk.Math.Max.max/1])
-      #Function<0.88898665/1 in Fxnk.Curry.curry/3>
-      iex> minmax.(1,3,5,7)
+      iex> minmax = Fxnk.Juxt.juxt([&Fxnk.Math.min/1, &Fxnk.Math.max/1])
+      iex> minmax.([1,3,5,7])
       [1, 7]
   """
+  @spec juxt([function(), ...]) :: fun()
   def juxt(fns) when is_list(fns) do
     curry(fn arg -> juxt(arg, fns) end)
   end
@@ -24,9 +24,10 @@ defmodule Fxnk.Juxt do
   `juxt/2` takes an initial argument and list of functions and applies the functions to the argument.
 
   ## Example
-      iex> Fxnk.Juxt.juxt([1, 3,5,7], [&Fxnk.Math.Min.min/1, &Fxnk.Math.Max.max/1])
+      iex> Fxnk.Juxt.juxt([1, 3, 5, 7], [&Fxnk.Math.min/1, &Fxnk.Math.max/1])
       [1, 7]
   """
+  @spec juxt(any, [function(), ...]) :: any
   def juxt(arg, fns) do
     perform_juxt(arg, fns, [])
   end
