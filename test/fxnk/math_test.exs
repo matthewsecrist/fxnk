3eb8208b6a6d1661829f29e1997fcb859831dbec
Matthew Secrist <matthew@matthewsecrist.net>
2019-09-07 20:33:33 -0500

    100% test coverage (#8)

--- test/fxnk/math_test.exs
+++ test/fxnk/math_test.exs
@@ -1,4 +1,11 @@
 defmodule Fxnk.MathTest do
   use ExUnit.Case
   doctest Fxnk.Math
+
+  import Fxnk.Math
+
+  test "finds the maximum of a list" do
+    assert 3 = max([1, 2, 3])
+    assert 1 = max([1])
+  end
 end
