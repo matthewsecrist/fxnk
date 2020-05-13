b90f9cc5be24dfb048daa6d4410f25a02926b07a
Matthew Secrist <matthew@matthewsecrist.net>
2020-05-12 22:47:15 -0500

    Add test watcher, add always, tap, prop and props.

--- lib/fxnk/map.ex
+++ lib/fxnk/map.ex
@@ -5,6 +5,72 @@ defmodule Fxnk.Map do
   import Fxnk.Functions, only: [curry: 1]
 
   @doc """
+  `prop/1`
+
+  Accepts a string `key` and returns a function that takes a `map`.
+  Returns the map's value at `key` or `nil`.
+
+  ## Examples
+      iex> getProp = Fxnk.Map.prop("foo")
+      iex> getProp.(%{"foo" => "foo", "bar" => "bar"})
+      "foo"
+      iex> getProp2 = Fxnk.Map.prop(:foo)
+      iex> getProp2.(%{foo: "foo", bar: "bar"})
+      "foo"
+  """
+  def prop(key) do
+    curry(fn map -> prop(map, key) end)
+  end
+
+  @doc """
+  `prop/2`
+
+  Accepts a map and a key. Returns the map's value at `key` or `nil`
+
+  ## Examples
+      iex> Fxnk.Map.prop(%{"foo" => "foo", "bar" => "bar"}, "foo")
+      "foo"
+      iex> Fxnk.Map.prop(%{foo: "foo", bar: "bar"}, :foo)
+      "foo"
+  """
+  def prop(map, key) when is_map(map) do
+    map[key]
+  end
+
+  @doc """
+  `props/1`
+
+  Accepts a list of keys and returns a function that takes a map.
+  Returns a list of the values associated with the keys in the map.
+
+  ## Examples
+      iex> getProps = Fxnk.Map.props(["foo", "bar"])
+      iex> getProps.(%{"foo" => "foo", "bar" => "bar", "baz" => "baz"})
+      ["foo", "bar"]
+      iex> getProps2 = Fxnk.Map.props([:foo, :bar])
+      iex> getProps2.(%{foo: "foo", bar: "bar", baz: "baz"})
+      ["foo", "bar"]
+  """
+  def props(keys) when is_list(keys) do
+    curry(fn map -> props(map, keys) end)
+  end
+
+  @doc """
+  `props/2`
+
+  Accepts a map and a list of keys and returns a list of the values associated with the keys in the map.
+
+  ## Examples
+      iex> Fxnk.Map.props(%{"foo" => "foo", "bar" => "bar", "baz" => "baz"}, ["foo", "bar"])
+      ["foo", "bar"]
+      iex> Fxnk.Map.props(%{foo: "foo", bar: "bar", baz: "baz"}, [:foo, :bar])
+      ["foo", "bar"]
+  """
+  def props(map, keys) when is_list(keys) and is_map(map) do
+    for key <- keys, do: prop(map, key)
+  end
+
+  @doc """
   `pick/1`
 
   Accepts a list of args, returns a curried `pick/2`.
@@ -28,7 +94,7 @@ defmodule Fxnk.Map do
       %{red: "RED", blue: "BLUE"}
   """
   @spec pick(map, [atom(), ...]) :: map
-  def pick(map, _) when map_size(map) == 0, do: %{}
+  def pick(map, _) when map_size(map) == 0, do: map
   def pick(_, []), do: %{}
 
   def pick(map, args) when is_map(map) and is_list(args) do
