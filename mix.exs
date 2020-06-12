266a18f1bb7ec5e7f3eba343de8df2fd0f4882dc
Matthew Secrist <matthew@matthewsecrist.net>
2020-06-11 21:55:58 -0500

    Publish version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fxnk.MixProject do
   def project do
     [
       app: :fxnk,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
