4114c4545ce13b6089ab201974519d11804e71df
Matthew Secrist <matthew@matthewsecrist.net>
2020-06-11 21:57:10 -0500

    remove excoveralls from production code

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Fxnk.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.9.0"},
+      {:excoveralls, "~> 0.9.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
