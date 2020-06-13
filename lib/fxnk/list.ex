538ec310ed9ffa7f8e92190cb0d1b60ee7217271
Matthew Secrist <matthew@matthewsecrist.net>
2020-06-13 12:25:50 -0500

    Add reduce_map, reduce_map_right

--- lib/fxnk/list.ex
+++ lib/fxnk/list.ex
@@ -29,8 +29,36 @@ defmodule Fxnk.List do
   """
   @spec zip_map(list(any), list(any)) :: [%{any() => any()}]
   def zip_map(list, keys) do
-    list
-    |> Enum.zip(keys)
-    |> Enum.map(fn {v, k} -> %{k => v} end)
+    keys
+    |> Enum.zip(list)
+    |> Enum.map(fn {k, v} -> %{k => v} end)
+  end
+
+  @doc """
+  Takes two lists and returns a map where the keys are the elements from the first list and the values are the elements from the second.
+
+  ## Examples
+      iex> Fxnk.List.reduce_map([1, 2, 3], [:one, :two, :three])
+      %{one: 1, two: 2, three: 3}
+  """
+  @spec reduce_map([any(), ...], [any()]) :: %{any() => any()}
+  def reduce_map(values, keys) do
+    keys
+    |> Enum.zip(values)
+    |> Enum.reduce(%{}, fn {key, value}, acc ->
+      Map.merge(acc, %{key => value})
+    end)
+  end
+
+  @doc """
+  Takes two lists and returns a map where the keys are the elements from the second list and the values are the elements from the first.
+
+  ## Examples
+      iex> Fxnk.List.reduce_map_right([:one, :two, :three], [1, 2, 3])
+      %{one: 1, two: 2, three: 3}
+  """
+  @spec reduce_map_right([any()], [any(), ...]) :: %{any() => any()}
+  def reduce_map_right(keys, values) do
+    reduce_map(values, keys)
   end
 end
